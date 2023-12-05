49dedab881da530d78447e1d9d1318afcb1d5519
Babiuc Ghenadie <babiuc.ghenadii@gmail.com>
2023-12-05 23:22:28 +0200

    Rebuilding the table to work with a vector

--- main.cpp
+++ main.cpp
@@ -1,8 +1,162 @@
 #include <iostream>
 #include <string>
 #include <stack>
+#include <vector>
 
 using namespace std;
+
+void TruthTable(int numberOfOperators, const vector<string> &operations);
+
+bool eval(const string &expression);
+
+bool evaluateExpression(string expression, int *values, int n);
+
+int main() {
+    cout << "Negatie (NOT):\n";
+    TruthTable(1, {"!A"});
+    cout << "\nConjunctie (AND):\n";
+    TruthTable(2, {"A & B"});
+    cout << "\nDisjunctie (OR):\n";
+    TruthTable(2, {"A | B"});
+    cout << "\nDiferenta (XOR):\n";
+    TruthTable(2, {"A ^ B"});
+    cout << "\nDiferenta simetrica (XNOR):\n";
+    TruthTable(2, {"!(A ^ B)"});
+    cout << "\n(NOR):\n";
+    TruthTable(2, {"!(A | B)"});
+    cout << "\n(NAND):\n";
+    TruthTable(2, {"!(A & B)"});
+    cout << "\nImplicatia (A -> B):\n";
+    TruthTable(2, {"((!A) | B)"});
+    cout << "\nEchivalenta (A <=> B):\n";
+    TruthTable(2, {"(A = B)"});
+    cout << "\nProblema Nr.1:\n";
+    TruthTable(3, {"A & B", "!(A & C)", "C ^ B", "(A & B) & (!(C & A)) & (C ^ B)"});
+    cout << "\nProblema Nr.2:\n";
+    TruthTable(4, {"A ^ B", "A ^ B ^ C", "A & B", "A & B & D", "!(A & B & D)", "(A ^ B ^ C) & (!(A & B & D))"});
+
+    return 0;
+}
+
+void TruthTable(int numberOfOperators, const vector<string> &operations) {
+    int listSize = operations.size();
+    //------------------------------------
+    printf("%c", 201);
+    for (int i = 0; i < numberOfOperators; i++) {
+        printf("%c%c%c%c", 205, 205, 205, 209);
+    }
+
+    for (int i = 0; i < listSize; i++) {
+        if (i != listSize - 1) {
+            printf("%c", 205);
+            for (int j = 0; j < operations[i].size() + 1; j++) {
+                printf("%c", 205);
+            }
+            printf("%c", 209);
+        } else {
+            for (int j = 0; j < operations[i].size() + 2; j++) {
+                printf("%c", 205);
+            }
+        }
+    }
+    printf("%c\n", 187);
+    //------------------------------------
+    printf("%c", 186);
+    int op = 65;
+    for (int i = 0; i < numberOfOperators; i++) {
+        printf(" %c %c", op, 179);
+        op++;
+    }
+    for (int i = 0; i < listSize; i++) {
+        if (i != listSize - 1) {
+            printf(" %s %c", operations[i].c_str(), 179);
+        } else {
+            printf(" %s %c\n", operations[i].c_str(), 186);
+        }
+    }
+    //------------------------------------
+    printf("%c", 199);
+    for (int i = 0; i < numberOfOperators; i++) {
+        printf("%c%c%c%c", 196, 196, 196, 197);
+    }
+    for (int i = 0; i < listSize; i++) {
+        if (i != listSize - 1) {
+            printf("%c", 196);
+            for (int j = 0; j < operations[i].size() + 1; j++) {
+                printf("%c", 196);
+            }
+            printf("%c", 197);
+        } else {
+            for (int j = 0; j < operations[i].size() + 2; j++) {
+                printf("%c", 196);
+            }
+        }
+    }
+    printf("%c\n", 182);
+    //------------------------------------
+    int rowCount = 1 << numberOfOperators;
+    int colCount = numberOfOperators;
+    int lines[rowCount][colCount];
+    for (int i = 0; i < (1 << numberOfOperators); ++i) {
+        printf("%c", 186);
+        int z = 0;
+        for (int j = numberOfOperators - 1; j >= 0; --j) {
+            int bit = (i >> j) & 1;
+            printf(" %d %c", bit, 179);
+            lines[i][z] = bit;
+            z++;
+        }
+
+        for (int j = 0; j < listSize; j++) {
+            if (j != listSize - 1) {
+                int spaces = operations[j].size() % 2 == 0 ? operations[j].size() / 2 : operations[j].size() / 2 + 1;
+
+                for (int c = 0; c < spaces; c++) {
+                    cout << " ";
+                }
+
+                cout << evaluateExpression(operations[j], lines[i], numberOfOperators);
+
+                for (int x = 0; x < operations[j].size() / 2 + 1; x++) {
+                    cout << " ";
+                }
+                printf("%c", 179);
+            } else {
+                int spaces = operations[j].size() % 2 == 0 ? operations[j].size() / 2 : operations[j].size() / 2 + 1;
+
+                for (int c = 0; c < spaces; c++) {
+                    cout << " ";
+                }
+
+                cout << evaluateExpression(operations[j], lines[i], numberOfOperators);
+
+                for (int x = 0; x < operations[j].size() / 2 + 1; x++) {
+                    cout << " ";
+                }
+                printf("%c\n", 186);
+            }
+        }
+    }
+    //------------------------------------
+    printf("%c", 200);
+    for (int i = 0; i < numberOfOperators; i++) {
+        printf("%c%c%c%c", 205, 205, 205, 207);
+    }
+    for (int i = 0; i < listSize; i++) {
+        if (i != listSize - 1) {
+            printf("%c", 205);
+            for (int j = 0; j < operations[i].size() + 1; j++) {
+                printf("%c", 205);
+            }
+            printf("%c", 207);
+        } else {
+            for (int j = 0; j < operations[i].size() + 2; j++) {
+                printf("%c", 205);
+            }
+        }
+    }
+    printf("%c\n", 188);
+}
 
 // Function to evaluate logical expressions
 bool eval(const string &expression) {
@@ -165,96 +319,4 @@ bool evaluateExpression(string expression, int *values, int n) {
     }
 
     return eval(expression);
-}
-
-void TruthTable(int numberOfOperators, string operation) {
-    //------------------------------------
-    printf("%c", 201);
-    for (int i = 0; i < numberOfOperators; i++) {
-        printf("%c%c%c%c", 205, 205, 205, 209);
-    }
-    for (int i = 0; i < operation.size() + 2; i++) {
-        printf("%c", 205);
-    }
-    printf("%c\n", 187);
-    //------------------------------------
-    printf("%c", 186);
-    int op = 65;
-    for (int i = 0; i < numberOfOperators; i++) {
-        printf(" %c %c", op, 179);
-        op++;
-    }
-    printf(" %s %c\n", operation.c_str(), 186);
-    //------------------------------------
-    printf("%c", 199);
-    for (int i = 0; i < numberOfOperators; i++) {
-        printf("%c%c%c%c", 196, 196, 196, 197);
-    }
-    for (int i = 0; i < operation.size() + 2; i++) {
-        printf("%c", 196);
-    }
-    printf("%c\n", 182);
-    //------------------------------------
-    int rowCount = 1 << numberOfOperators;
-    int colCount = numberOfOperators;
-    int lines[rowCount][colCount];
-    for (int i = 0; i < (1 << numberOfOperators); ++i) {
-        printf("%c", 186);
-        int z = 0;
-        for (int j = numberOfOperators - 1; j >= 0; --j) {
-            int bit = (i >> j) & 1;
-            printf(" %d %c", bit, 179);
-            lines[i][z] = bit;
-            z++;
-        }
-
-        int spaces = operation.size() % 2 == 0 ? operation.size() / 2 : operation.size() / 2 + 1;
-
-        for (int c = 0; c < spaces; c++) {
-            cout << " ";
-        }
-
-        cout << evaluateExpression(operation, lines[i], numberOfOperators);
-
-        for (int x = 0; x < operation.size() / 2 + 1; x++) {
-            cout << " ";
-        }
-
-        printf("%c\n", 186);
-    }
-    //------------------------------------
-    printf("%c", 200);
-    for (int i = 0; i < numberOfOperators; i++) {
-        printf("%c%c%c%c", 205, 205, 205, 207);
-    }
-    for (int i = 0; i < operation.size() + 2; i++) {
-        printf("%c", 205);
-    }
-    printf("%c\n", 188);
-}
-
-int main() {
-    cout << "Negatie (NOT):\n";
-    TruthTable(1, "!A");
-    cout << "\nConjunctie (AND):\n";
-    TruthTable(2, "A & B");
-    cout << "\nDisjunctie (OR):\n";
-    TruthTable(2, "A | B");
-    cout << "\nDiferenta (XOR):\n";
-    TruthTable(2, "A ^ B");
-    cout << "\nDiferenta simetrica (XNOR):\n";
-    TruthTable(2, "!(A ^ B)");
-    cout << "\n(NOR):\n";
-    TruthTable(2, "!(A | B)");
-    cout << "\n(NAND):\n";
-    TruthTable(2, "!(A & B)");
-    cout << "\nImplicatia (A -> B):\n";
-    TruthTable(2, "((!A) | B)");
-    cout << "\nEchivalenta (A <=> B):\n";
-    TruthTable(2, "(A = B)");
-
-    TruthTable(3, "A & B | (!C)");
-
-    TruthTable(4, "!(A & B ^ (!C) | D)");
-    return 0;
 }
